863eb63901e53755902cf797e6e9776e66d177b0
LAST <100422108+AlexseyLAST@users.noreply.github.com>
2024-09-11 15:20:48 +0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector(".nav__menu");
+const burger = document.querySelector(".burger")
+
+burger.addEventListener('click', function(){
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+})
